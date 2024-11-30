1d491eee8715fdf031f027be3d0c9413a78bcb65
Estudiante <zambranauma@gmail.com>
2024-11-30 19:58:53 +0100

    Inicios de sesión casi terminado (Falta validar la contraseña y cambiar_credenciales.php)

--- tienda/usuario/registro.php
+++ tienda/usuario/registro.php
@@ -10,30 +10,67 @@
         ini_set("display_errors", 1 );    
 
         require('../util/conexion.php');
+        session_start();
+        if (isset($_SESSION["usuario"])) {
+            header("location: iniciar_sesion.php");
+            exit;    
+        }
     ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
+    <div class="container">
     <?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $usuario = $_POST["usuario"];
-        $contrasena = $_POST["contrasena"];
+        $tmp_usuario = $_POST["usuario"];
+        $tmp_contrasena = $_POST["contrasena"];
+        /*Vas por aquí, ibas a empezar con la validación tanto de usuario como 
+        contraseña y lo que tienes ahí abajo tienes que ponerlo con el método isset de
+        contrasena_cifrada y usuario, ten en cuenta que empiezas con las variables
+        temporales tmp_<variable> 
+        Mejora: usa el strcmp para hacer una doble validación de la contraseña en el 
+        registro del usuario, y si no coinciden, pues no te deja crearlo.
+        */
+        $sql = "SELECT * FROM usuarios WHERE usuario = '$tmp_usuario'";
+            $resultado = $_conexion -> query($sql);
 
-        $contrasena_cifrada = password_hash($contrasena,PASSWORD_DEFAULT);
-
-        $sql = "INSERT INTO usuarios VALUES ('$usuario','$contrasena_cifrada')";
-        $_conexion -> query($sql);
-
-        header("location: iniciar_sesion.php");
-        exit;
+        if ($tmp_usuario == '') {
+            $err_usuario = "El usuario es obligatorio";
+        }
+        else {
+            if($resultado -> num_rows != 0) {
+                $err_usuario = "El usuario $tmp_usuario ya existe.";
+            } 
+            else {
+                if (strlen($tmp_usuario) < 3 || strlen($tmp_usuario) > 15) {
+                    $err_usuario = "El usuario debe tener entre 3 y 15 caracteres";
+                }
+                else {
+                    $patron = "/^[a-zA-Z0-9áéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ ]{3,15}$/";
+                    if (!preg_match($patron, $tmp_usuario)) {
+                        $err_usuario = "El usuario sólo puede tener letras y números.";
+                    }
+                    else {
+                        $usuario = $tmp_usuario;
+                    }
+                }
+            }
+        }
+        
+        $contrasena_cifrada = password_hash($tmp_contrasena,PASSWORD_DEFAULT);
     }
     ?>
-    <div class="container">
         <h1>Registro</h1>
         
         <form class="col-6" action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label class="form-label">Usuario</label>
                 <input class="form-control" type="text" name="usuario">
+                <?php if(isset($err_usuario)) echo "<span class='error'>$err_usuario</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Contraseña</label>
@@ -48,6 +85,13 @@
             <a class="btn btn-secondary" href="iniciar_sesion.php">Iniciar sesión</a>
         </div>
     </div>
+    <?php
+        if (isset($usuario) && isset($contrasena_cifrada)) {
+            $sql = "INSERT INTO usuarios VALUES ('$usuario','$contrasena_cifrada')";
+            $_conexion -> query($sql);
+            header("location: iniciar_sesion.php");
+        }
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
